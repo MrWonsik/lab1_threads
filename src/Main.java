9e32fcee8cab013b58940ae3d50b9d2bce97390c
MrWonsik <MrWonsik@users.noreply.github.com>
2019-10-23 16:04:45 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
 import executor.Task;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Executor exec = Executors.newFixedThreadPool(2);
-        for(int i=1; i <= 4 ;i++){
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+        for(int i=1; i <= 10 ;i++){
             exec.execute(new Task("Task" + i));
         }
+        Thread.yield();
+        exec.shutdown();
+
+        try {
+            exec.execute(new Task("Task po zamknieciu"));
+        } catch (RejectedExecutionException e)
+        {
+            e.printStackTrace();
+        }
+        try {
+            exec.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(exec.isTerminated());
+
     }
 }
--- src/executor/Task.java
+++ src/executor/Task.java
@@ -12,14 +12,14 @@ public class Task implements Runnable {
 
     @Override
     public void run() {
-        for(int i = 1; i<=4; i++) {
-            System.out.println(name + " " + i);
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Thread.yield();
+
+        System.out.println(name);
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        Thread.yield();
     }
+
 }
